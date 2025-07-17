15000125d16a2c9e21346062bf4f53f2e7020b23
hdfaouz <hdfaouzia2@gmail.com>
2025-07-17 10:10:24 +0100

    Creation de Model pour User

--- auth-service/src/main/java/com/enaa/authservice/repository/UserRepository.java
+++ auth-service/src/main/java/com/enaa/authservice/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.enaa.authservice.repository;
 
-public interface UserRepository {
+import com.enaa.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
