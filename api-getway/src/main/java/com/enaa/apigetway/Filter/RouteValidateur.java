b555e0367488006820184148ebf23dc13a9db97e
hdfaouz <hdfaouzia2@gmail.com>
2025-07-17 10:50:55 +0100

    création des classes et modification dans dependencies

--- api-getway/src/main/java/com/enaa/apigetway/Filter/RouteValidateur.java
+++ api-getway/src/main/java/com/enaa/apigetway/Filter/RouteValidateur.java
@@ -1,4 +1,23 @@
 package com.enaa.apigetway.Filter;
 
-public class RouteValidateur {
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidateur  {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/register",
+            "/auth/token",
+            "/eureka"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
 }
